964ca9a71d45eefac48eacd4d27431ccbe569c22
Alex Matsoukas <amatsoukas@olin.edu>
2023-11-02 01:06:04 -0400

    update docstring for main function

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from trainedModel import trainedModel
 def main():
     """
     Call classes that collect data, train a model, and predict in real time.
+
+    Args:
+        N/A
+    Returns:
+        N/A
     """
     extractor = keypointExtractor()
     modelInstance = trainedModel()
